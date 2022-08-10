f7e37cb863f9cfd817c2c66b55e83be57988bd07
Muhammed Hassan M <muhammed.m@quantiphi.com>
2022-08-10 21:10:14 +0530

    Read Config

--- src/pipeline_1_data_preparation.py
+++ src/pipeline_1_data_preparation.py
@@ -3,13 +3,25 @@ import argparse
 import yaml
 import logging
 
+def read_parameters(config_path):
+    
+    with open(config_path)as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def main(config_path,data_source):
+    config = read_parameters(config_path)
+    print(config)
+
 if __name__ =="__main__":
     # Find related codes in mlops_main
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="default")
-    args.add_argument("--data_source",default = None)
+    default_config_path = os.path.join('config','params.yaml')
+    args.add_argument("--config",default=default_config_path)
+    args.add_argument("--datasource",default = None)
     
     parsed_args = args.parse_args()
     print(parsed_args.config)
-    print(parsed_args.data_source)
+    # print(parsed_args.data_source)
+    main(config_path = parsed_args.config,data_source = parsed_args.datasource)
     
